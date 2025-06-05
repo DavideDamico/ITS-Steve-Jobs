ab7247c2c1945ec8cbf5f9f2234c1f39285e011b
DavideDamico <davidedamico89@gmail.com>
2025-06-05 13:36:13 +0200

    Creazione index esercizio cocktail

---
+++ SteveJobs/WEB-FRONTEND/JavaScript/esercitazioneAPI05-06/esercizio_cocktail/script.js
@@ -0,0 +1,22 @@
+// Esercizio: Cerca un cocktail e visualizza i dettagli
+// Obiettivo:
+// Creare una pagina web che:
+//     1.    Permetta all’utente di cercare un cocktail per nome.
+//     2.    Invia una richiesta all’API https://www.thecocktaildb.com/api/json/v1/1/search.php?s={nome}.
+//     3.    Mostri le seguenti informazioni sul primo risultato:
+//         o  Nome del cocktail
+//         o  Immagine
+//         o  Lista ingredienti + quantità
+//         o  Istruzioni per prepararlo
+// Requisiti:
+// ·      Campo di input per inserire il nome del cocktail.
+// ·      Bottone per avviare la ricerca.
+// ·      Area per visualizzare i dati.
+// ·      Gestione degli errori (cocktail non trovato, errore API).
+// Bonus:
+// ·      Mostrare tutti i risultati se ce ne sono più di uno.
+// ·      Permettere la ricerca anche per ingrediente (https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=gin).
+// ·      Integrare un dropdown con le categorie o tipi di bicchiere.
+// ·      Generare random cocktails.
+// 11:42
+// https://www.thecocktaildb.com/api.php
