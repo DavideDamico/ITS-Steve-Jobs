90e24985a6af6fbf8d3c86384b555f621d6b177e
Davide Damico <davidedamico89@gmail.com>
2025-10-14 10:59:05 +0200

    Nuova lezione su SQLTools e pratica

---
+++ SteveJobs/DATA-MANAGEMENT/Appunti_Lezioni/Lezione_13_10.md
@@ -0,0 +1,71 @@
+# Prove pratiche sul database 'pizzadb' tramite SQLTools
+
+SELECT * FROM coupon WHERE CouponID = 5;
+
+SELECT
+    ProductID,
+    ProductName,
+    ProductType,
+    Price,
+    (Price * 1.04) AS PriceMaggiorato
+FROM product
+
+SELECT
+    ProductID,
+    ProductName,
+    ProductType,
+    Price,
+    (Price * 1.04) AS PriceMaggiorato,
+    (Price * 1.04 - Price) AS ValoreMaggiorazione,
+    (CONCAT(ProductName, ' ', ProductType)) AS NomeCompleto
+FROM product
+
+Per arrotondare i numeri con la virgola usare 'ROUND':
+   ROUND(Price * 1.04, 2) AS PriceMaggiorato
+
+### Comando per i filtri
+ WHERE ProductType = 'PIZZA' OR ProductType = 'DESS'
+
+### Alternativa più veloce
+ WHERE ProductType IN ('PIZZA', 'DESS') 
+
+### Per escludere un tipo , diverso da
+ WHERE ProductType <> 'PIZZA'
+
+### Alternativa più veloce
+ WHERE ProductType NOT IN ('PIZZA', 'DESS')
+ WHERE Price <= 3 OR Price > 10
+ ORDER BY Price
+
+### Alternativa più veloce
+ WHERE Price BETWEEN 3 AND 10
+
+### Per cercare tramite la fine o l'inizio di lettere precise
+WHERE ProductType LIKE '%ri%'
+%ri = trova tutte le parole che finiscono con 'ri'
+ri% = trova tutte le parole che iniziano con 'ri' 
+%ri% = trova tutte le parole che hanno 'ri' al suo interno
+
+SELECT
+    CustomerName,
+    ProductName,
+    Count(*) AS NumeroOrdini
+FROM customerorderdetail
+GROUP BY CustomerName, ProductName
+
+Il count ci serve per sapere effettivamente quante volte quel prodotto è stato ordinato , perchè sennò non lo vedremmo
+
+![alt text](./Immagini/image-47.png)
+
+SELECT
+    e.EmployeeID,
+    e.FirstName,
+    e.LastName,
+    h.StartDate,
+    h.TerminationDate
+FROM Employee as e
+INNER JOIN employeehistory as h
+on e.EmployeeID = h.EmployeeID
+ORDER BY e.LastName, h.StartDate
+
+![alt text](./Immagini/image-48.png)
---
+++ SteveJobs/DATA-MANAGEMENT/XAMPP MYSQL (LOCAL).session.sql
@@ -0,0 +1,73 @@
+-- SELECT * FROM coupon WHERE CouponID = 5;
+
+-- SELECT
+--     ProductID,
+--     ProductName,
+--     ProductType,
+--     Price,
+--     ROUND(Price * 1.04, 2) AS PriceMaggiorato,
+--     ROUND(Price * 1.04 - Price, 2) AS ValoreMaggiorazione
+-- FROM product
+
+-- WHERE ProductType = 'PIZZA' OR ProductType = 'DESS'
+-- WHERE ProductType IN ('PIZZA', 'DESS') 
+-- WHERE ProductType <> 'PIZZA'
+-- WHERE ProductType NOT IN ('PIZZA', 'DESS')
+
+-- WHERE Price <= 3 OR Price > 10
+-- WHERE Price BETWEEN 3 AND 10
+
+-- WHERE ProductName LIKE '%ri%'
+
+-- WHERE Price > 3 
+-- ORDER BY Price DESC
+
+-- Esercizio:
+-- Vorrei osservare tutte le pizze in catalogo, contenenti nel nome la lettera i,
+-- ordinate in ordine alfabetico , nello specifico vorrei osservare il nome della pizza,
+-- il prezzo, ed il prezzo al netto dell'IVA al 4%
+
+-- SELECT
+--     ProductType,
+--     ProductName,
+--     Price,
+--     ROUND(Price * 0.96, 2) AS Prezzo_Netto
+-- FROM product
+-- WHERE ProductName LIKE '%i%'
+-- ORDER BY ProductType, ProductName;
+
+-- SELECT
+--     CustomerName,
+--     ProductName,
+--     Count(*) AS NumeroOrdini
+-- FROM customerorderdetail
+-- GROUP BY CustomerName, ProductName
+-- ORDER BY CustomerName asc, ProductName desc
+
+-- SELECT
+--     CustomerName,
+--     ProductName,
+--     SUM(PurchaseAmount) AS PurchaseAmount,
+--     Price * SUM(Quantity) AS TotalPurchaseAmountCalculated
+
+-- FROM customerorderdetail
+-- GROUP BY CustomerName, ProductName, Price
+-- ORDER BY CustomerName, ProductName;
+
+-- SELECT
+--     e.EmployeeID,
+--     e.FirstName,
+--     e.LastName,
+--     h.StartDate,
+--     h.TerminationDate
+-- FROM Employee as e
+-- INNER JOIN employeehistory as h
+-- on e.EmployeeID = h.EmployeeID
+-- ORDER BY e.LastName, h.StartDate
+
+SELECT
+    p.ProductID,
+    p.ProductName,
+    p.Price,
+    p.Price - (SELECT AVG(Price) FROM product)
+FROM product as p
