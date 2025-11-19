3324ca0f723fdbbfa6916233a2f609b9cc0b2410
Davide Damico <davidedamico89@gmail.com>
2025-11-19 16:52:28 +0100

    Creazione ambiente react-native con expo

---
+++ SteveJobs/REACT/react-native-corso-34/app/(tabs)/test.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const TestPage = () => {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text>Test Page</Text>
+        </View>
+    </SafeAreaView>
+  );
+}
+
+export default TestPage;
